c048f394219ae57ff6911bb0619473113cad903f
Khan From Chedum <khanfromchedum@elsewhere.com>
2021-10-30 16:07:06 +0200

    filterlist defaulted to null

--- RadioBrowser.net/RadioBrowser/RadioBrowserListOf.cs
+++ RadioBrowser.net/RadioBrowser/RadioBrowserListOf.cs
@@ -18,7 +18,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of tags</returns>
-        public async Task<List<StationCount>> GetTags(FilterListStationCount filterList)
+        public async Task<List<StationCount>> GetTags(FilterListStationCount filterList =null)
         {
             string strJson = await GetList("tags", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
@@ -28,7 +28,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of countries</returns>
-        public async Task<List<StationCount>> GetCountries(FilterListStationCount filterList)
+        public async Task<List<StationCount>> GetCountries(FilterListStationCount filterList=null)
         {
             string strJson = await GetList("countries", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
@@ -38,7 +38,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of country codes</returns>
-        public async Task<List<StationCount>> GetCountryCodes(FilterListStationCount filterList)
+        public async Task<List<StationCount>> GetCountryCodes(FilterListStationCount filterList=null)
         {
             string strJson = await GetList("countrycodes", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
@@ -48,7 +48,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of codecs</returns>
-        public async Task<List<StationCount>> GetCodecs(FilterListStationCount filterList)
+        public async Task<List<StationCount>> GetCodecs(FilterListStationCount filterList=null)
         {
             string strJson = await GetList("codecs", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
@@ -58,7 +58,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of states</returns>
-        public async Task<List<StationCount>> GetStates(FilterListStatesStationCount filterList)
+        public async Task<List<StationCount>> GetStates(FilterListStatesStationCount filterList=null)
         {
             string strJson = await GetList("states", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
@@ -68,7 +68,7 @@ namespace RadioBrowser
         /// </summary>
         /// <param name="filterList">filter to apply</param>
         /// <returns>list of languages</returns>
-        public async Task<List<StationCount>> GetLanguages(FilterListStationCount filterList)
+        public async Task<List<StationCount>> GetLanguages(FilterListStationCount filterList=null)
         {
             string strJson = await GetList("languages", filterList);
             return ConvertStationCount.GetStationCounts(strJson);
